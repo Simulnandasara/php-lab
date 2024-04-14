44ac6d5081d879ef49c83afd1f884dd0e28cf269
Simulnandasara <165984077+Simulnandasara@users.noreply.github.com>
2024-04-14 09:28:26 +0530

    hint.php

---
+++ hint.php
@@ -0,0 +1,41 @@
+<?php 
+$a[] = "Anna"; 
+$a[] = "Brittany"; 
+$a[] = "Cinderella"; 
+$a[] = "Diana"; 
+$a[] = "Eva"; 
+$a[] = "Fiona"; 
+$a[] = "Gunda"; 
+$a[] = "Hege"; 
+$a[] = "Inga"; 
+$a[] = "Johanna"; 
+$a[] = "Kitty"; 
+$a[] = "Linda"; 
+$a[] = "navdip"; 
+$a[] = "Ophelia"; 
+$a[] = "Petunia"; 
+$a[] = "Amanda"; 
+$a[] = "Raquel"; 
+$a[] = "Cindy"; 
+
+
+$q = $_REQUEST["q"]; 
+$hint = ""; 
+
+if ($q !== "") { 
+  $q = strtolower($q); 
+  $len=strlen($q); 
+
+foreach($a as $name) { 
+if (stristr($q, substr($name, 0, $len))) { 
+if ($hint === "") { 
+$hint = $name; 
+} else { 
+$hint .= ", $name"; 
+} 
+} 
+} 
+} 
+
+echo $hint === "" ? "no suggestion" : $hint; 
+?>
