5164a4f46be32f058dcb68a10970cce3e723fb4f
Simulnandasara <165984077+Simulnandasara@users.noreply.github.com>
2024-04-14 09:29:14 +0530

    40.php

--- 40.php
+++ 40.php
@@ -13,7 +13,7 @@ function showHint(str) {
         document.getElementById("txtHint").innerHTML = this.responseText; 
       } 
     } 
-    xmlhttp.open("GET", "gethint.php?q="+str, true); 
+    xmlhttp.open("GET", "hint.php?q="+str, true); 
     xmlhttp.send(); 
   } 
 } 
